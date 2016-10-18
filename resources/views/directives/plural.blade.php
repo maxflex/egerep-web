8cb30bf9f02f4c8fa913834b981ee0ad3a03353a
Max Kolyadin <makcyxa-k@yandex.ru>
2016-10-18 16:12:24 +0300

    Plural space

--- resources/views/directives/plural.blade.php
+++ resources/views/directives/plural.blade.php
@@ -1,4 +1,4 @@
-<span ng-hide='hideZero && !count' ng-if="!noneText || count || withoutDigit"><span ng-if='!textOnly'>@{{ count }}</span> <ng-pluralize count="count" when="{
+<span ng-hide='hideZero && !count' ng-if="!noneText || count || withoutDigit"><span ng-if='!textOnly'>@{{ count | number }}</span> <ng-pluralize count="count" when="{
         'one': when[type][0],
         'few': when[type][1],
         'many': when[type][2]
