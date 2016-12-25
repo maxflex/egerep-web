52f7ebeb2f193cdfe622988ba44c4a89c050002d
Max Kolyadin <makcyxa-k@yandex.ru>
2016-12-25 17:55:46 +0300

    Request log added

--- app/Http/Controllers/Api/RequestsController.php
+++ app/Http/Controllers/Api/RequestsController.php
@@ -9,11 +9,13 @@ use App\Http\Controllers\Controller;
 use App\Models\Service\Api;
 use App\Http\Requests\RequestStore;
 use App\Service\Limiter;
+use DB;
 
 class RequestsController extends Controller
 {
     public function store(RequestStore $request)
     {
+        DB::table('request_log')->insert($request->all());
         return Limiter::run('request', 24, 200, function() use ($request) {
             $_SESSION['sent_ids'][] = $request->tutor_id;
             Api::exec('requestNew', $request->input());
