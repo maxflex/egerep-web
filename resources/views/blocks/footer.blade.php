15caf2b0c4158d3140799c8b1cd2b79f0638b43d
shamshod <shamik1551@mail.ru>
2017-03-24 11:34:56 +0300

    - block list padding removed

--- resources/views/blocks/footer.blade.php
+++ resources/views/blocks/footer.blade.php
@@ -3,7 +3,7 @@
     <div>
         <b>{{ $block['title'] }}</b>
     </div>
-    <ul class='footer-links' ng-init="toggler_{{ $loop->index }} = {{ count($block['links']) }} > {{ \App\Models\Page::MAX_BLOCK_LINKS_MOBILE }}; show_block_items_{{ $loop->index }} = !toggler_{{$loop->index}}">
+    <ul class='footer-links npb' ng-init="toggler_{{ $loop->index }} = {{ count($block['links']) }} > {{ \App\Models\Page::MAX_BLOCK_LINKS_MOBILE }}; show_block_items_{{ $loop->index }} = !toggler_{{$loop->index}}">
         @foreach($block['links'] as $link)
             <li @if ($loop->index > \App\Models\Page::MAX_BLOCK_LINKS_MOBILE) ng-show="show_block_items_{{ $loop->parent->index }}" @endif>
                 <a href='{{ $link->url }}'>{{ trim($link->anchor) ? $link->anchor : $link->getClean('h1') }}</a>
