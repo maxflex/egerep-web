0235eeb21f1ff09687c8f7172bbaf3c66c33c03a
maxflex <makcyxa-k@yandex.ru>
2018-07-24 12:15:40 +0300

    rating style update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14012,23 +14012,6 @@ c){g.push("<a ");h.isDefined(b)&&g.push('target="',b,'" ');g.push('href="',a.rep
 }).call(this);
 
 (function() {
-  angular.module('Egerep').value('Genders', {
-    male: 'мужской',
-    female: 'женский'
-  }).value('Sources', {
-    LANDING: 'landing',
-    LANDING_PROFILE: 'landing_profile',
-    LANDING_HELP: 'landing_help',
-    FILTER: 'filter',
-    PROFILE_REQUEST: 'profilerequest',
-    SERP_REQUEST: 'serprequest',
-    HELP_REQUEST: 'helprequest',
-    MORE_TUTORS: 'more_tutors'
-  });
-
-}).call(this);
-
-(function() {
   angular.module('Egerep').directive('ngAge', function() {
     return {
       restrict: 'A',
@@ -14248,6 +14231,7 @@ c){g.push("<a ");h.isDefined(b)&&g.push('target="',b,'" ');g.push('href="',a.rep
           'lesson': ['занятие', 'занятия', 'занятий'],
           'client': ['клиент', 'клиента', 'клиентов'],
           'mark': ['оценки', 'оценок', 'оценок'],
+          'marks': ['оценка', 'оценки', 'оценок'],
           'review': ['отзыв', 'отзыва', 'отзывов'],
           'request': ['заявка', 'заявки', 'заявок'],
           'station': ['станцию', 'станции', 'станций'],
@@ -14500,6 +14484,23 @@ c){g.push("<a ");h.isDefined(b)&&g.push('target="',b,'" ');g.push('href="',a.rep
       }
     };
   };
+
+}).call(this);
+
+(function() {
+  angular.module('Egerep').value('Genders', {
+    male: 'мужской',
+    female: 'женский'
+  }).value('Sources', {
+    LANDING: 'landing',
+    LANDING_PROFILE: 'landing_profile',
+    LANDING_HELP: 'landing_help',
+    FILTER: 'filter',
+    PROFILE_REQUEST: 'profilerequest',
+    SERP_REQUEST: 'serprequest',
+    HELP_REQUEST: 'helprequest',
+    MORE_TUTORS: 'more_tutors'
+  });
 
 }).call(this);
 
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -928,23 +928,6 @@
 }).call(this);
 
 (function() {
-  angular.module('Egerep').value('Genders', {
-    male: 'мужской',
-    female: 'женский'
-  }).value('Sources', {
-    LANDING: 'landing',
-    LANDING_PROFILE: 'landing_profile',
-    LANDING_HELP: 'landing_help',
-    FILTER: 'filter',
-    PROFILE_REQUEST: 'profilerequest',
-    SERP_REQUEST: 'serprequest',
-    HELP_REQUEST: 'helprequest',
-    MORE_TUTORS: 'more_tutors'
-  });
-
-}).call(this);
-
-(function() {
   angular.module('Egerep').directive('ngAge', function() {
     return {
       restrict: 'A',
@@ -1164,6 +1147,7 @@
           'lesson': ['занятие', 'занятия', 'занятий'],
           'client': ['клиент', 'клиента', 'клиентов'],
           'mark': ['оценки', 'оценок', 'оценок'],
+          'marks': ['оценка', 'оценки', 'оценок'],
           'review': ['отзыв', 'отзыва', 'отзывов'],
           'request': ['заявка', 'заявки', 'заявок'],
           'station': ['станцию', 'станции', 'станций'],
@@ -1416,6 +1400,23 @@
       }
     };
   };
+
+}).call(this);
+
+(function() {
+  angular.module('Egerep').value('Genders', {
+    male: 'мужской',
+    female: 'женский'
+  }).value('Sources', {
+    LANDING: 'landing',
+    LANDING_PROFILE: 'landing_profile',
+    LANDING_HELP: 'landing_help',
+    FILTER: 'filter',
+    PROFILE_REQUEST: 'profilerequest',
+    SERP_REQUEST: 'serprequest',
+    HELP_REQUEST: 'helprequest',
+    MORE_TUTORS: 'more_tutors'
+  });
 
 }).call(this);
 
